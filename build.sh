473fe474135e25a571dcc802eb7d16175bf0aa65
FiluFafayo <postdisposable2@gmail.com>
2025-09-18 14:25:40 +0700

    build copy qpdf local

--- build.sh
+++ build.sh
@@ -7,17 +7,16 @@ echo ">>> Installing qpdf and its dependencies..."
 yum install -y qpdf
 
 echo ">>> Creating a directory for our binaries inside the API folder..."
-# Kita buat folder _bin di dalam /api agar ikut di-deploy
 mkdir -p api/_bin
 
 echo ">>> Copying qpdf executable and required libraries..."
 # Salin program utamanya
 cp /usr/bin/qpdf api/_bin/
 
-# Salin "onderdil" (shared libraries) yang dibutuhkan qpdf
-# Build log menunjukkan qpdf-libs diinstall, biasanya isinya ada di /usr/lib64
-cp /usr/lib64/libqpdf.so.29 api/_bin/
-cp /usr/lib64/libjpeg.so.62 api/_bin/
+# SALIN DENGAN WILDCARD (*) UNTUK MENEMUKAN VERSI YANG BENAR SECARA OTOMATIS
+# Ini akan menyalin file seperti libqpdf.so.29, libqpdf.so.30, dll.
+cp /usr/lib64/libqpdf.so.* api/_bin/
+cp /usr/lib64/libjpeg.so.* api/_bin/
 
 echo ">>> All binaries and libraries copied."
 
