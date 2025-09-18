0fdef39dbfd1a0714f997c0d08e5916ccc2e7329
FiluFafayo <postdisposable2@gmail.com>
2025-09-18 16:15:45 +0700

    Perbaiki kredensial thumbnail

--- api/get-thumbnail.js
+++ api/get-thumbnail.js
@@ -61,7 +61,10 @@ export default async function handler(req, res) {
 
     try {
         const auth = new google.auth.GoogleAuth({
-            credentials: { /* ... kredensialmu ... */ },
+            credentials: {
+                client_email: process.env.GOOGLE_CLIENT_EMAIL,
+                private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+            },
             scopes: ['https://www.googleapis.com/auth/drive.readonly'],
         });
         const drive = google.drive({ version: 'v3', auth });
